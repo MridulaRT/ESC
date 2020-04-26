2a9c762bf5756cb41b151980da4c517faea6c41b
MridulaRT <49071305+MridulaRT@users.noreply.github.com>
2020-04-26 16:02:44 +0800

    translation.js

--- translation.js
+++ translation.js
@@ -1,105 +1,4 @@
-/* basically the Agent and Customer classes */
-class Agent {
-    constructor(id, name, skills, product, availability, queue) {
-        // details from contacts
-        this.id = id,
-            this.name = name,
-            this.skills = skills,
-            this.product = product,
-            this.availability = availability,
-            this.queue = queue;
-    }
-}
-;
-
-class Customer {
-    constructor(first, last, product, issue, agent) {
-        // details from the guest accounts
-        this.firstname = first,
-            this.lastname = last,
-            // details from the form
-            this.product = product,
-            this.issue = issue,
-            // log this somewhere?
-            this.agent = agent;
-    }
-}
-;
-
-/* checks if the agent has <5 customers in the queue before being considered by the algorithm
-   also sorts the list of agents according to most free to least free */
-function consider_available_agents(agentList) {
-    var availableAgents = [];
-    for (agent of agentList) {
-        if ((agent.availability == true) && agent.queue.length < 5) {
-            availableAgents.push(agent);
-        }
-    }
-    availableAgents.sort(function(a, b) {return (a.queue.length - b.queue.length)});
-    return availableAgents;
-}
-
-// dummy database (need to figure out how to read json file and get details from rainbow)
-var database = {
-    agents: [
-        {
-            id: '123',
-            name: 'Joshua',
-            email: 'jyeoyi@gmail.com',
-            password: 'Mahalo',
-            entries: 0,
-            product: 'Ephone',
-            expertise: 'hardware',
-            available: false,
-            custqueue: []
-        },
-        {
-            id: '21',
-            name: 'ZhaoYi', 
-            email: 'tesh@yahoo.com.sg',
-            password: 'barmitzvah',
-            entries: 21,
-            product: 'Epad',
-            expertise: 'replacement of parts',
-            available: true,
-            custqueue: ['cs1', 'cs2']
-        },
-        {
-            id: '456',
-            name: 'Sandeep', 
-            email: 'donaldTrump@potus.com',
-            password: 'trump',
-            entries: 668,
-            product: 'Emac',
-            expertise: 'maintainance of parts',
-            available: true,
-            custqueue: []
-        }
-    ],
-    customers: [
-        {
-            id: '1003456',
-            name: 'Jack',
-            product: 'Ephone',
-            issue: 'maintainance of parts',
-            agent: undefined
-        },
-        {
-            id: '1003789',
-            name: 'Jill',
-            product: 'Ephone',
-            issue: 'hardware',
-            agent: undefined
-        },
-        {
-            id: '1003123',
-            name: 'James',
-            issue: 'others',
-            agent: undefined
-        }
-    ]
-}
-
+const cnf = require('./classes.js');
 var express = require('express');
 var app = express();
 app.listen(3001,()=>{
@@ -122,16 +21,16 @@ app.post('/form', (req, res)=>{
 
     /* make the agentList and customer instance here*/
     var agentList = [];
-    for (agent of database.agents) {
-        agentList.push(new Agent(agent.id, agent.name, agent.expertise, agent.product, agent.available, agent.custqueue));
+    for (agent of cnf.database.agents) {
+        agentList.push(new cnf.Agent(agent.id, agent.name, agent.expertise, agent.product, agent.available, agent.custqueue));
     }
     //console.log(agentList);
 
-    var customer = new Customer(guestFirstName, guestLastName, product, issue);
+    var customer = new cnf.Customer(guestFirstName, guestLastName, product, issue);
     console.log(customer);
 
     /* agent assignment happens here*/
-    for (agent of consider_available_agents(agentList)) {
+    for (agent of cnf.available(agentList)) {
         if ((agent.skills == customer.issue) && (agent.product == customer.product)) {
             customer.agent = agent.name;
             console.log(customer.agent);
@@ -145,6 +44,3 @@ app.post('/form', (req, res)=>{
         console.log("No available agents");
     }
 });
-
-
-
